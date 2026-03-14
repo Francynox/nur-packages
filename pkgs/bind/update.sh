3767adea5df69d23cbf45666228d32b29d35e44f
Francesco Benini <francy.benini@gmail.com>
2026-03-14 15:52:41 +0100

    fix: update pup selector for bind and kea update script

--- pkgs/bind/update.sh
+++ pkgs/bind/update.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 PKG_NAME="bind"
 PAGE_URL="https://www.isc.org/download/#BIND"
-PUP_SELECTOR='div#BIND :parent-of(td:contains("Development")) td:first-child text{}'
+PUP_SELECTOR='div#BIND td.download-version[title*="testing"] .download-version-text text{}'
 
 version=$(fetch_version_html "$PAGE_URL" "$PUP_SELECTOR")
 run_nix_update "$PKG_NAME" "$version"
--- pkgs/kea/update.sh
+++ pkgs/kea/update.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 PKG_NAME="kea"
 PAGE_URL="https://www.isc.org/download/#Kea"
-PUP_SELECTOR='div#Kea :parent-of(td:contains("Development")) td:first-child text{}'
+PUP_SELECTOR='div#Kea td.download-version[title*="testing"] .download-version-text text{}'
 
 version=$(fetch_version_html "$PAGE_URL" "$PUP_SELECTOR")
 run_nix_update "$PKG_NAME" "$version"
